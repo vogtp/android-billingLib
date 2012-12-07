62736a90398cd3a22d455e7099ff28676cbad3fb
Patrick S. Vogt <patrick.vogt.pv@gmail.com>
2012-12-07 18:59:33 +0100

    rename

--- src/ch/almana/android/billing/products/BuyMeABeerProductsInitialiser.java
+++ src/ch/almana/android/billing/products/BuyMeABeerProductsInitialiser.java
@@ -1,17 +1,18 @@
 package ch.almana.android.billing.products;
 
 import android.content.Context;
+import ch.almana.android.billing.ProductManager;
 import ch.almana.android.billing.R;
 import ch.almana.android.billing.backend.BillingManager.Managed;
 
-public class BuyMeABeerProducts {
+public class BuyMeABeerProductsInitialiser {
 
 	public static final int PRODUCTS_BUYMEABEER = 4711;
 
-	private static boolean initialised = false;
-	protected static ProductManager productManager;
+	private boolean initialised = false;
+	protected ProductManager productManager;
 
-	public static void initProducts(Context ctx) {
+	public void initProducts(Context ctx) {
 		if (!initialised) {
 			initialised = true;
 			productManager = ProductManager.getInstance(ctx);
@@ -19,11 +20,11 @@ public class BuyMeABeerProducts {
 		}
 	}
 
-	protected static void inititaliseProductLists(Context ctx) {
+	protected void inititaliseProductLists(Context ctx) {
 		productManager.addProducts(PRODUCTS_BUYMEABEER, getBuyMeABeerProducts(ctx));
 	}
 
-	public static ProductList getBuyMeABeerProducts(Context ctx) {
+	private ProductList getBuyMeABeerProducts(Context ctx) {
 		ProductList beer = new ProductList(new Product("kids.cookie", ctx.getString(R.string.name_buy_cookie), ctx.getString(R.string.desc_buy_cookie), Managed.UNMANAGED),
 				new Product("bar.beer", ctx.getString(R.string.name_buy_me_a_beer), ctx.getString(R.string.desc_buy_beer), Managed.UNMANAGED));
 		beer.add(new Product("bar.whiskey", ctx.getString(R.string.name_buy_me_a_whiskey), ctx.getString(R.string.desc_buy_whiskey), Managed.UNMANAGED));
